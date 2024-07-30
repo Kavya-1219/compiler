70282187f0559ec1cad0c5d55421ae6d06c3f14b
Kavya-1219 <132338691+Kavya-1219@users.noreply.github.com>
2024-07-30 14:49:42 +0530

    Update exp39 common subexpression elimination.c

--- exp39 common subexpression elimination.c
+++ exp39 common subexpression elimination.c
@@ -1,48 +1,37 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
-int evaluateExpression(const char *expr) {
-    int a, b;
-    char op;
-
-    sscanf(expr, "%d %c %d", &a, &op, &b);
-
-    switch (op) {
-        case '+': return a + b;
-        case '-': return a - b;
-        case '*': return a * b;
-        case '/': return a / b;
-        default: return 0;
-    }
-}
-
-void eliminateCommonSubexpressions(const char *expr) {
-    char subExpr1[] = "5 * 3";
-    char subExpr2[] = "5 * 3"; 
-    char subExpr3[] = "2 + 4";
-
-    int result1 = evaluateExpression(subExpr1);
-    int result3 = evaluateExpression(subExpr3);
-
-    printf("Original Expression: %s\n", expr);
-    printf("Optimized Expression: \n");
-    printf("temp1 = %d\n", result1); 
-    printf("result = temp1 + %d\n", result3); 
-}
+#define MAX_EXPR_LEN 100
 
 int main() {
-    char expr[100];
+    char expressions[][MAX_EXPR_LEN] = {
+        "x + y",
+        "y * z",
+        "x + y", 
+        "x / z",
+    };
+    int num_expr = sizeof(expressions) / sizeof(expressions[0]);
 
-    printf("Enter an arithmetic expression: ");
-    fgets(expr, sizeof(expr), stdin);
-
-    size_t length = strlen(expr);
-    if (length > 0 && expr[length - 1] == '\n') {
-        expr[length - 1] = '\0';
+    printf("Original Code:\n");
+    for (int i = 0; i < num_expr; ++i) {
+        printf("%s\n", expressions[i]);
     }
 
-    eliminateCommonSubexpressions(expr);
+    
+    for (int i = 0; i < num_expr; ++i) {
+        for (int j = i + 1; j < num_expr; ++j) {
+            if (strcmp(expressions[i], expressions[j]) == 0) {
+                strcpy(expressions[j], "; Removed"); 
+            }
+        }
+    }
+
+    printf("\nOptimized Code:\n");
+    for (int i = 0; i < num_expr; ++i) {
+        if (strcmp(expressions[i], "; Removed") != 0) {
+            printf("%s\n", expressions[i]);
+        }
+    }
 
     return 0;
 }
